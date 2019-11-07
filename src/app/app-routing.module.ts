4c4f00f32e323b81779a49ecb83667e95a6fa819
ana-valdivia <ana.raquel1291@hotmail.com>
2019-11-06 23:03:06 -0200

    NOVO PROJETO

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,29 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ListaProjetosComponent } from './lista-projetos/lista-projetos.component';
+import { NovoProjetosComponent } from './novo-projetos/novo-projetos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+    {
+      path : 'projetos',
+      component : ListaProjetosComponent
+           
+
+     
+
+    },
+
+    {
+      path : 'novoprojeto',
+      component : NovoProjetosComponent
+    }
+
+
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
