e77ee4bbad841aafa257f0709ec870f9fbcbf256
ana-valdivia <ana.raquel1291@hotmail.com>
2019-11-06 23:22:48 -0200

    Criando detalhes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ListaProjetosComponent } from './lista-projetos/lista-projetos.component';
 import { NovoProjetosComponent } from './novo-projetos/novo-projetos.component';
+import { DetalheProjetosComponent } from './detalhe-projetos/detalhe-projetos.component';
 
 const routes: Routes = [
 
@@ -17,7 +18,13 @@ const routes: Routes = [
     {
       path : 'novoprojeto',
       component : NovoProjetosComponent
-    }
+    },
+
+    {
+      path : 'detalheprojeto/:id',
+      component : DetalheProjetosComponent
+      }.
+      
 
 
 
--- src/app/lista-projetos/lista-projetos.component.ts
+++ src/app/lista-projetos/lista-projetos.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'has-lista-projetos',
@@ -9,9 +10,13 @@ export class ListaProjetosComponent implements OnInit {
    projetos = [{"nome":"projeto 1","sala":"lab8","turma":"2Q"},
                {"nome":"projeto 2","sala":"lab7","turma":"3Q"}]
               ;
-  constructor() { }
+  constructor(private router : Router) { }
 
   ngOnInit() {
+  }
+
+  detalhes(id:number){
+    this.router.navigate(['detalheprojeto',id]);
   }
 
 }
